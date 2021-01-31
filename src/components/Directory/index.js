8ac6f44bc71616fe86cefe572099ecb1e0057d0a
Bruno Carvalho <160221028@estudantes.ips.pt>
2021-01-31 22:38:44 +0000

    homepage and directory display first page images

--- src/components/Directory/index.js
+++ src/components/Directory/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import Cards from './../../assets/cards.jpg';
+import Other from './../../assets/other.jpg';
+import './styles.scss';
 
 const Directory = props => {
     return (
-        <div>
-
+      <div className="directory">
+        <div className="wrap">
+          <div
+            className="item"
+            style={{
+              backgroundImage: `url(${Cards})`
+            }}
+          >
+            <a >
+              ShopCards
+            </a>
+          </div>
+          <div
+            className="item"
+            style={{
+              backgroundImage: `url(${Other})`
+            }}
+          >
+            <a>
+              Shop Other
+            </a>
+          </div>
         </div>
+      </div>
     );
-};
-
-export default Directory;
+  };
+  
+  export default Directory;
