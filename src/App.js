3cdb08085e27d5e45547c3337d563c9177d60049
Bruno Carvalho <160221028@estudantes.ips.pt>
2021-02-09 17:03:17 +0000

    Refactoring with Redux Hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {Switch, Route,  Redirect} from 'react-router-dom';
 import { auth, handleUserProfile } from './firebase/utils';
 import { setCurrentUser } from './redux/User/user.actions';
@@ -23,7 +23,7 @@ import './default.scss';
 
 const App =  props => {
 
-  const { setCurrentUser, currentUser } = props;
+  const dispatch = useDispatch();
 
   useEffect(() => {
 
@@ -31,14 +31,14 @@ const App =  props => {
       if(userAuth){
         const userRef = await handleUserProfile(userAuth);
         userRef.onSnapshot(snapshot => {
-          setCurrentUser({
+          dispatch(setCurrentUser({
             id: snapshot.id,
             ...snapshot.data()
-          });
+          }));
         })
       }
 
-      setCurrentUser(userAuth);
+      dispatch(setCurrentUser(userAuth));
     });
     return () => {
       authListener();
@@ -85,12 +85,4 @@ const App =  props => {
   );
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
-});
-
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/components/EmailPassword/index.js
+++ src/components/EmailPassword/index.js
@@ -1,43 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { resetPassword, resetAllAuthForms } from './../../redux/User/user.actions';
+
 import './styles.scss';
-
-import { auth } from './../../firebase/utils';
-
 import AuthWrapper from './../AuthWrapper';
 import FormInput from './../forms/FormInput';
 import Button from './../forms/Button'; 
 
+const mapState = ({ user }) => ({
+    resetPasswordSuccess: user.resetPasswordSuccess,
+    resetPasswordError: user.resetPasswordError
+});
+
 const EmailPassword = props => {
+    const { resetPasswordSuccess, resetPasswordError } = useSelector(mapState);
+    const dispatch = useDispatch();
     const [email, setEmail] = useState('');
     const [errors, setErrors] = useState([]);
+
+    useEffect(() =>{
+        if(resetPasswordSuccess){
+            reset();
+            dispatch(resetAllAuthForms());
+            props.history.push('/login');
+        }
+    }, [resetPasswordSuccess]);
+    
+    useEffect(() =>{
+        if(Array.isArray(resetPasswordError) && resetPasswordError.length > 0){
+            setErrors(resetPasswordError);
+        }
+    }, [resetPasswordError]);
 
     const reset = () => {
         setEmail('');
         setErrors([]);
     }
 
-    const handleSubmit = async e =>{
+    const handleSubmit = e =>{
         e.preventDefault();
-
-        const config = {
-            url: 'http://localhost:3000/login'
-        }
-        
-        try {
-            
-            await auth.sendPasswordResetEmail(email, config)
-                .then(() => {
-                    //console.log('Password Reset');
-                    props.history.push('/login');
-                }).catch(() => {
-                    //console.log('Something went wrong');
-                    const err =['Email not found. Please try again.'];
-                    setErrors(err);
-                });
-        } catch (err) {
-            // console.log(err);
-        }
+        dispatch(resetPassword({ email }));
     }
 
     const configAuthWrapper = {
